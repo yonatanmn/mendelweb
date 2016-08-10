4efd8ddc17cb215abbb918e5c86835779facda0d
yonatan s <yonatanmn@gmail.com>
2016-08-11 01:52:26 +0300

    some coloring

--- mandel.js
+++ mandel.js
@@ -1,9 +1,11 @@
 "use strict";
 
-var iterations = 400;
-const escapeRadius = 2;
+var iterations = 200;
+const escapeRadius = 2 ;
+const memo = {};
 
 function isInMandelForLoop(Cr, Ci) {
+  // if(memo['a'+Cr+'b'+Ci]) {return memo['a'+Cr+'b'+Ci]}
   var Zr = 0;
   var Zi = 0;
   var Tr = 0;
@@ -23,7 +25,9 @@ function isInMandelForLoop(Cr, Ci) {
     Ti = Zi * Zi;
   }
   // return [n, Tr, Ti];
-  return n;
+  // memo['a'+Cr+'b'+Ci] = {n, Tr, Ti};
+
+  return {n, Tr, Ti};
 }
 
 var ctx, img;
@@ -47,7 +51,7 @@ function mapToRGB(num, in_min, in_max){
 }
 
 function handleOnWheelScroll(e) {
-  console.log(e);
+  // console.log(e);
   // console.log(e.wheelDelta);
   // console.log(e.which);
   if(!e.wheelDelta) {return}
@@ -114,16 +118,19 @@ function render() {
 
   for (let i = canvas.height; i > 0; i--) {
     for (let j = canvas.width; j > 0; j--, pixels ++) {
-      const numOfIter = isInMandelForLoop(
+      const res = isInMandelForLoop(
         leftIndent + mapToZoom(canvas.width - j, 0, canvas.width),
         topIndent + mapToZoom(canvas.height - i, 0, canvas.height)
       );
-      const color = /*255 -*/ mapToRGB(numOfIter, 0, iterations);
+      // const color = /*255 -*/ mapToRGB(res.n, 0, iterations);
+      const color = /*255 -*/ mapToRGB(res.n - Math.log(Math.sqrt(res.Tr+res.Ti)), 0, iterations);
 
-
-      img.data[pixels*4 ] = color;
-      img.data[pixels*4 + 1] = color;
-      img.data[pixels*4 + 2] = color;
+      var v = 1 + res.n - Math.log(Math.log(Math.sqrt(res.Tr+res.Ti)))/Math.log(2.0);
+      var v1 = res.n - Math.log(Math.log(Math.sqrt(res.Tr+res.Ti)))/Math.log(2.0);
+      // console.log(v);
+      img.data[pixels*4 ] = mapToRGB(v1 + res.Ti*2500, 0, iterations);
+      img.data[pixels*4 + 1] = mapToRGB(v1 * res.n, 0, iterations);
+      img.data[pixels*4 + 2] = mapToRGB(v1, 0, iterations);
       img.data[pixels*4 + 3] = 255;
       // pixels ++;
     }
