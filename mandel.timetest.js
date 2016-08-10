1dc82ef9abb302ec50ba0de60e55272123444c36
yonatan s <yonatanmn@gmail.com>
2016-08-10 14:35:47 +0300

    add time tests for both functions

---
+++ mandel.timetest.js
@@ -0,0 +1,17 @@
+var counter = 10000;
+
+iterations = 500000
+
+var STARTisInMandel = Date.now();
+for (let i = 0; i < counter; i++) {
+    isInMandel(0.2 + i/counter/10)
+}
+
+console.log('isInMandel', Date.now() - STARTisInMandel);
+
+var STARTisInMandelForLoop = Date.now();
+for (let i = 0; i < counter; i++) {
+  isInMandelForLoop(0.2 + i/counter/10)
+}
+
+console.log('isInMandelForLoop', Date.now() - STARTisInMandelForLoop);
